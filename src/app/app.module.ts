71a49a7d66ae8a8656297ad0d919e4d12b417ef8
Emiliano D'Alesio <edalesio@baufest.com>
2017-11-09 15:08:26 -0300

    Se agrego el provide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { AbmPage } from '../pages/abm/abm';
 import { AdmPerfilPage } from '../pages/adm-perfil/adm-perfil';
@@ -15,6 +14,7 @@ import { GraficoEncuestaPage } from '../pages/grafico-encuesta/grafico-encuesta'
 import { MainPage } from '../pages/main/main';
 import { QRsPage } from '../pages/q-rs/q-rs';
 import { HomePage } from '../pages/home/home';
+import { ApiabmProvider } from '../providers/apiabm/apiabm';
 @NgModule({
   declarations: [
     MyApp,
@@ -50,7 +50,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ApiabmProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,22 +1,53 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {MainPage} from '../main/main';
+import { MainPage } from '../main/main';
+import { ApiabmProvider } from '../../providers/apiabm/apiabm';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
+  providers: [ApiabmProvider]
 })
 export class HomePage {
-usuario:string;
-pass:string;
+  usuario: string;
+  pass: string;
   constructor(public navCtrl: NavController) {
 
   }
-  login()
-  {
-    if(this.usuario=="admin" && this.pass=="123" || this.usuario=="alumno" && this.pass=="11")
-    {
-      this.navCtrl.setRoot(MainPage, {"usuario":this.usuario,"pass":this.pass});
+  login() {
+    if (this.usuario == "admin" && this.pass == "123" || this.usuario == "alumno" && this.pass == "11") {
+      this.navCtrl.setRoot(MainPage, { "usuario": this.usuario, "pass": this.pass });
     }
   }
 
+  asignarUsuario(tipo) {
+    switch (tipo) {
+      case "admin": {
+        this.usuario = "admin";
+        this.pass = "11";
+        break;
+      }
+      case "invitado": {
+        this.usuario = "invitado";
+        this.pass = "22";
+        break;
+      }
+      case "usuario": {
+        this.usuario = "usuario";
+        this.pass = "33";
+        break;
+      }
+      case "j1": {
+        this.usuario = "j1";
+        this.pass = "44";
+        break;
+      }
+      case "j2": {
+        this.usuario = "j2";
+        this.pass = "55";
+        break;
+      }
+    }
+
+  }
 }
