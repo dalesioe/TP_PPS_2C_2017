aec142989ee2736fd1bb4017ecd0f1cf6b17d423
Gui <guillermo.fink@bsg.global>
2017-11-11 17:45:23 -0300

    act_base

--- pps.sql
+++ pps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-11-2017 a las 13:22:14
+-- Tiempo de generación: 11-11-2017 a las 21:44:52
 -- Versión del servidor: 10.1.24-MariaDB
 -- Versión de PHP: 7.1.6
 
@@ -27,28 +27,92 @@ USE `pps`;
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `cursada`
+-- Estructura de tabla para la tabla `asistencias`
 --
 
-CREATE TABLE `cursada` (
-  `id_materia` int(11) NOT NULL,
-  `id_alumno` int(11) NOT NULL,
-  `1` int(11) NOT NULL,
-  `2` int(11) NOT NULL,
-  `3` int(11) NOT NULL,
-  `4` int(11) NOT NULL,
-  `5` int(11) NOT NULL,
-  `6` int(11) NOT NULL,
-  `7` int(11) NOT NULL,
-  `8` int(11) NOT NULL,
-  `9` int(11) NOT NULL,
-  `10` int(11) NOT NULL,
-  `11` int(11) NOT NULL,
-  `12` int(11) NOT NULL,
-  `13` int(11) NOT NULL,
-  `14` int(11) NOT NULL,
-  `15` int(11) NOT NULL
+CREATE TABLE `asistencias` (
+  `id_asistencia` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `asistencias`
+--
+
+INSERT INTO `asistencias` (`id_asistencia`, `id_curso`, `fecha`) VALUES
+(0, 1, '2017-11-07');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comisiones`
+--
+
+CREATE TABLE `comisiones` (
+  `id_comision` int(11) NOT NULL,
+  `nombre` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `comisiones`
+--
+
+INSERT INTO `comisiones` (`id_comision`, `nombre`) VALUES
+(1, '2A');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso`
+--
+
+CREATE TABLE `curso` (
+  `id_curso` int(11) NOT NULL,
+  `id_comision` int(11) NOT NULL,
+  `id_profesor` int(11) NOT NULL,
+  `id_materia` int(11) NOT NULL,
+  `dia` int(11) NOT NULL,
+  `aula` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `curso`
+--
+
+INSERT INTO `curso` (`id_curso`, `id_comision`, `id_profesor`, `id_materia`, `dia`, `aula`) VALUES
+(1, 1, 7, 4, 3, 205);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_asistencia`
+--
+
+CREATE TABLE `detalle_asistencia` (
+  `id_asistencia` int(11) NOT NULL,
+  `id_alumno` int(11) NOT NULL,
+  `presente` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_curso`
+--
+
+CREATE TABLE `detalle_curso` (
+  `id_curso` int(11) NOT NULL,
+  `id_alumno` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `detalle_curso`
+--
+
+INSERT INTO `detalle_curso` (`id_curso`, `id_alumno`) VALUES
+(1, 11),
+(1, 15);
 
 -- --------------------------------------------------------
 
@@ -59,9 +123,18 @@ CREATE TABLE `cursada` (
 CREATE TABLE `materias` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
-  `fecha_inicio` date NOT NULL,
   `aula` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `materias`
+--
+
+INSERT INTO `materias` (`id`, `nombre`, `aula`) VALUES
+(1, 'MATEMATICA I', 403),
+(2, 'MATEMATICA II', 201),
+(3, 'PROGRAMACION', 21),
+(4, 'PROGRAMACION II', 22);
 
 -- --------------------------------------------------------
 
@@ -89,14 +162,39 @@ INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `sexo`, `dni`, `mail`, `pass
 (1, 'guillermo', 'fink', 'M', 32022487, 'guillermo_fink@hotmail.com', 321321321, 1, 1),
 (3, 'juan pablo', 'letteri', 'M', 31225658, 'jp_lt@gmail.com', 321321321, 2, 1),
 (6, 'emiliano', 'dalesio', 'M', 30256985, 'dalesioe@gmail.com', 321321321, 3, 1),
-(7, 'octavio', 'villegas', 'M', 25132659, 'octavio@gmail.com', 321321321, 4, 2),
-(8, 'roxana', 'gonzalez', 'F', 22549865, 'rx@gmail.com', 321321321, 5, 3),
-(11, 'raul', 'perez', 'M', 36252485, 'rp@gmail.com', 321321321, 1001, 4),
-(12, 'roberto', 'gomez', 'M', 34521695, 'robert@gmail.com', 321321321, 1002, 4);
+(7, 'octavio', 'villegas', 'M', 25132659, 'octavio@gmail.com', 321321321, 1004, 2),
+(8, 'roxana', 'gonzalez', 'F', 22549865, 'rx@gmail.com', 321321321, 105, 3),
+(11, 'raul', 'perez', 'M', 36252485, 'rp@gmail.com', 321321321, 100001, 4),
+(12, 'roberto', 'gomez', 'M', 34521695, 'robert@gmail.com', 321321321, 100002, 4),
+(15, 'Juan', 'Perez', 'M', 20154895, 'juan@yahoo.com.ar', 20154895, 100003, 4);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `asistencias`
+--
+ALTER TABLE `asistencias`
+  ADD PRIMARY KEY (`id_asistencia`,`id_curso`);
+
+--
+-- Indices de la tabla `comisiones`
+--
+ALTER TABLE `comisiones`
+  ADD PRIMARY KEY (`id_comision`);
+
+--
+-- Indices de la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`id_curso`);
+
+--
+-- Indices de la tabla `detalle_curso`
+--
+ALTER TABLE `detalle_curso`
+  ADD PRIMARY KEY (`id_curso`,`id_alumno`);
 
 --
 -- Indices de la tabla `materias`
@@ -115,15 +213,25 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `comisiones`
+--
+ALTER TABLE `comisiones`
+  MODIFY `id_comision` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT de la tabla `materias`
 --
 ALTER TABLE `materias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
