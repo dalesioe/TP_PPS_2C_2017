4c16f920b0dae12865a4b79acf584b7debe361e5
juanpabloletteri <pabloletteri@gmail.com>
2017-11-16 00:42:51 -0300

    menu eleccion asistencia traido de la base (materia profesor aula dia)

--- src/pages/asistencia/asistencia.ts
+++ src/pages/asistencia/asistencia.ts
@@ -19,29 +19,18 @@ export class AsistenciaPage {
   usuario: string;
   pass: string;
 
-  ape_prof: string;
-  Comision: string;
-  Materia: string;
-  Dia: string;
-  Aula: string;
+  Materia: number;
+  Profesor: number;
+  Aula: number;
+  Dia: number;
 
   materias: any;
   profesores: any;
-  lista: any;
-  cursos: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
     this.usuario = this.navParams.get('usuario');
     this.pass = this.navParams.get('pass');
-    /////////////TRAER CURSOS///////////////
-    this.http.get("http://www.estacionamiento.16mb.com/git/api/traerCursosPorDia")
-      .subscribe(data => {
-        this.cursos = data.json();
-        console.log(data['_body']);
-      }, error => {
-        console.log(error);// Error getting the data
-      });
-    /////////////TRAER MATERIAS///////////////
+    /////////////TRAER MATERIAS Y AULAS///////////////
     this.http.get("http://www.estacionamiento.16mb.com/git/api/traerTodasLasMaterias")
       .subscribe(data => {
         this.materias = data.json();
