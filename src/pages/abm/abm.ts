5bea0e27d98b001b43800a4ec8ac98d4b1a94e6e
Emiliano D'Alesio <edalesio@baufest.com>
2017-12-08 17:44:15 -0300

    abm alumnos con archivo ok

--- src/pages/abm/abm.ts
+++ src/pages/abm/abm.ts
@@ -4,6 +4,7 @@ import { MainPage } from '../main/main';
 import { Http } from '@angular/http';
 import { File } from '@ionic-native/file';
 import { AlertController } from 'ionic-angular';
+import * as papa from 'papaparse';
 //import 'rxjs/add/operator/map';
 /**
  * Generated class for the AbmPage page.
@@ -18,6 +19,7 @@ import { AlertController } from 'ionic-angular';
   templateUrl: 'abm.html',
 })
 export class AbmPage {
+  archivotitulo:string;
   archivos: any;
   lista: any;
   Importacion: any;
@@ -37,11 +39,34 @@ export class AbmPage {
   Fecha: Date;
   existe: boolean;
   tipoUsuario: string;
+  api: string;
   constructor(private alertCtrl: AlertController, public file: File, public navCtrl: NavController, public navParams: NavParams, public http: Http) {
     this.usuario = this.navParams.get('usuario');
     this.pass = this.navParams.get('pass');
     this.tipo = this.navParams.get('tipo');
+    this.definirApi();
     this.tipoUsuario = this.navParams.get('tipoUsuario');
+    this.http.get("http://www.estacionamiento.16mb.com/git/api/traerArchivos")
+      .subscribe(data => {
+        this.archivos = data.json();
+      }, error => {
+        console.log(error);
+      });
+  }
+  definirApi() {
+    switch (this.tipo) {
+      case "2":
+        this.api = "http://www.estacionamiento.16mb.com/git/api/altaAdministrativo";
+        break;
+      case "3":
+        this.api = "http://www.estacionamiento.16mb.com/git/api/altaProfesor";
+        break;
+      case "4":
+        this.api = "http://www.estacionamiento.16mb.com/git/api/altaAlumno";
+        break;
+      default:
+        break;
+    }
   }
   mostrarAlert(exito) {
     if (exito == 1) {
@@ -62,9 +87,109 @@ export class AbmPage {
     }
   }
 
-  AltaConArchivo() {
-   
+  AltaConArchivoAlumno() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Elegir archivo');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Alumno',
+      value: 'Alumno',
+      checked: true
+    });
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'Ok',
+      handler: data => {
+        console.log('Radio data:', data);
+        this.testRadioOpen = false;
+        this.testRadioResult = data;
+
+        if (data) {
+          switch (data) {
+            case 'Alumno':
+              this.archivo = 'Alumnos1.csv';
+              this.ComprobarArchivo();
+              break;
+          }
+        }
+      }
+    });
+    alert.present().then(() => {
+      this.testRadioOpen = true;
+    });
   }
+  ComprobarArchivo() {
+    if(this.archivo==this.archivotitulo)
+    {
+      this.existe=true;
+    }
+    this.archivos.forEach(element => {
+      if (element.titulo == this.archivo){
+        this.existe = true;
+        return;
+      }
+    })
+
+    if (!this.existe) {
+      this.readCsvData();
+    } else {
+      this.AlertMensaje("El archivo ya fue importado", "Archivo duplicado");
+      this.existe = false;
+    }
+  }
+  public readCsvData() {
+    this.http.get('assets/archivos/' + this.archivo)
+      .subscribe(
+      data => this.extractData(data),
+      err => this.handleError(err)
+      );
+  }
+  private extractData(res) {
+    let csvData = res['_body'] || '';
+    let parsedData = papa.parse(csvData).data;
+
+    this.headerRow = parsedData[0];
+
+    parsedData.splice(0, 1);
+    this.csvData = parsedData;
+
+    this.guardarLista();
+  }
+  public guardarLista() {
+    //Guardamos la Importacion
+    let datos = { "titulo": this.archivo };
+    this.http.post("http://www.estacionamiento.16mb.com/git/api/subirArchivo", datos).subscribe(
+      data => console.log(data)
+    );
+    for (var index = 0; index < this.csvData.length - 1; index++) {
+      var element = this.csvData[index];
+      var nombre = element[0];
+      var apellido = element[1];
+      var mail = element[2];
+      var dni = element[3];
+      var sexo = element[4];
+      this.lista = {
+        "nombre": nombre, "apellido": apellido, "mail": mail, "dni":dni, "sexo": sexo
+      };
+      this.http.post(this.api, this.lista).subscribe(
+        data => console.log(data)
+      );
+    }
+    this.existe = false;
+    this.AlertMensaje("El archivo fue agregado exitosamente!", "Proceso finalizado");
+    this.archivotitulo= this.archivo;
+    this.http.get("http://www.estacionamiento.16mb.com/git/api/traerArchivos")
+      .subscribe(data => {
+        this.archivos = data.json();
+      }, error => {
+        console.log(error);
+      });
+  }
+  private handleError(err) {
+    console.log('something went wrong: ', err);
+  }
+
   Alta() {
     let body: any;
     body = { "nombre": this.nombre, "apellido": this.apellido, "mail": this.mail, "dni": this.dni, "sexo": this.sexo };
@@ -79,33 +204,33 @@ export class AbmPage {
           });
         break;
 
-      case "2":
+      case "3":
         this.http.post('http://www.estacionamiento.16mb.com/git/api/altaAdministrativo', body)
-        .subscribe(data => {
-          this.mostrarAlert(1);
-          this.Volver();
-        }, error => {
-          this.mostrarAlert(0);// Error getting the data
+          .subscribe(data => {
+            this.mostrarAlert(1);
+            this.Volver();
+          }, error => {
+            this.mostrarAlert(0);// Error getting the data
           });
         break;
 
-      case "3":
+      case "2":
         this.http.post("http://www.estacionamiento.16mb.com/git/api/altaProfesor", body)
-        .subscribe(data => {
-          this.mostrarAlert(1);
-          this.Volver();
-        }, error => {
-          this.mostrarAlert(0);// Error getting the data
+          .subscribe(data => {
+            this.mostrarAlert(1);
+            this.Volver();
+          }, error => {
+            this.mostrarAlert(0);// Error getting the data
           });
         break;
 
       case "4":
         this.http.post("http://www.estacionamiento.16mb.com/git/api/altaAlumno", body)
-        .subscribe(data => {
-          this.mostrarAlert(1);
-          this.Volver();
-        }, error => {
-          this.mostrarAlert(0);// Error getting the data
+          .subscribe(data => {
+            this.mostrarAlert(1);
+            this.Volver();
+          }, error => {
+            this.mostrarAlert(0);// Error getting the data
           });
         break;
 
@@ -114,7 +239,23 @@ export class AbmPage {
     }
   }
   Volver() {
-    this.navCtrl.setRoot(MainPage, { "usuario": this.usuario, "pass": this.pass, "tipo":this.tipoUsuario });
+    this.navCtrl.setRoot(MainPage, { "usuario": this.usuario, "pass": this.pass, "tipo": this.tipoUsuario });
   }
+  AlertMensaje(titulo: string, mens: string) {
 
+    let ventana = this.alertCtrl.create({
+      title: titulo,
+      message: mens,
+      buttons: [
+        {
+          text: "Aceptar",
+          handler: data => {
+            console.log('Mensaje de Alerta');
+          }
+        }
+      ]
+
+    });
+    ventana.present(ventana);
+  }
 }
