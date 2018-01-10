eae1346cffe4eadd1b7c3c53a828dd075c3534ad
juanpabloletteri <pabloletteri@gmail.com>
2018-01-10 20:27:09 -0300

    traducciono modificacion page OK

--- src/pages/modificacion/modificacion.ts
+++ src/pages/modificacion/modificacion.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { Http } from '@angular/http';
+import { TranslateService } from '@ngx-translate/core';
 /**
  * Generated class for the ModificacionPage page.
  *
@@ -21,8 +22,33 @@ export class ModificacionPage {
   dni: number;
   mail: string;
 
+  idioma: string;
+  felicitaciones: string;
+  cambiodedatos: string;
+  reiniciopass: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController,
-    public http: Http) {
+    public http: Http, public traductor: TranslateService) {
+    ////////IDIOMA//////////////
+    this.idioma = this.traductor.currentLang;
+    switch (this.idioma) {
+      case "es":
+        this.felicitaciones = "Felicitaciones!";
+        this.cambiodedatos = "Se han cambiado los datos del usuario de manera exitosa!";
+        this.reiniciopass = "Se reiniciado su contraseña de manera exitosa!";
+        break;
+      case "en":
+        this.felicitaciones = "Congratulations!";
+        this.cambiodedatos = "The user data has been changed successfully!";
+        this.reiniciopass = "Your password was successfully restarted!";
+        break;
+      case "pt":
+        this.felicitaciones = "Parabéns!";
+        this.cambiodedatos = "Os dados do usuário foram alterados com sucesso!";
+        this.reiniciopass = "Sua senha foi reiniciada com sucesso!";
+        break;
+    }
+    ///////////////////////////////
     this.id = this.navParams.get('id');
     this.nombre = this.navParams.get('nombre');
     this.apellido = this.navParams.get('apellido');
@@ -44,8 +70,8 @@ export class ModificacionPage {
     );
     ///////////alert//////
     let alert = this.alertCtrl.create({
-      title: 'Felicitaciones!',
-      subTitle: 'Se han cambiado los datos del usuario de manera exitosa!',
+      title: this.felicitaciones,
+      subTitle: this.cambiodedatos,
       buttons: ['OK']
     });
     alert.present();
@@ -62,8 +88,8 @@ export class ModificacionPage {
     );
     ///////////alert//////
     let alert = this.alertCtrl.create({
-      title: 'Felicitaciones!',
-      subTitle: 'Se reiniciado su contraseña de manera exitosa!',
+      title: this.felicitaciones,
+      subTitle: this.reiniciopass,
       buttons: ['OK']
     });
     alert.present();
